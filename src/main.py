d364d719fc1714959d881ddf50c235fae4b64094
Cory Blankenship <cory.r.blankenship.ctr@nga.mil>
2026-03-27 12:48:28 -0400

    Updated reference to tqdm package

--- src/main.py
+++ src/main.py
@@ -407,7 +407,7 @@ def _run_waf_audit(
     all_results = []
     total = len(successes)
     iterable = (
-        tqdm(successes, desc="Auditing policies", unit="policy")
+        _tqdm(successes, desc="Auditing policies", unit="policy")
         if _HAS_TQDM
         else successes
     )
